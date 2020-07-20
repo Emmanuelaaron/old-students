2636e5dd9b1ab1a0c0e1921cb023717eb03b4a60
emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-07-21 02:28:26 +0300

    Adds styling to the layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :alumnis, only: [:new, :create, :edit, :update, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "alumnis#new"
 end
