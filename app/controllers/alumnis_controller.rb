048b19006da9a243513afa4b4d3fe3c8249a9611
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2022-03-04 20:13:08 +0300

    - sets up linters - changes the user interface

--- app/controllers/alumnis_controller.rb
+++ app/controllers/alumnis_controller.rb
@@ -1,73 +1,75 @@
 class AlumnisController < ApplicationController
-    def new
-        @alumni = Alumni.new
-    end
+  def new
+    @alumni = Alumni.new
+  end
 
-    def create
-        @alumni = Alumni.create(alumni_params)
-        if @alumni.save
-          AlumniMailer.with(alumni: @alumni).welcome_email.deliver_now
-          redirect_to @alumni
-        else
-          render 'new'
-        end
+  def create
+    @alumni = Alumni.create(alumni_params)
+    if @alumni.save
+      AlumniMailer.with(alumni: @alumni).welcome_email.deliver_now
+      redirect_to @alumni
+    else
+      render 'new'
     end
+  end
 
-    def show
-        @alumni = Alumni.find(params[:id])
-    end
+  def show
+    @alumni = Alumni.find(params[:id])
+  end
 
-    def index
-        @alumnis = Alumni.all
-    end
+  def index
+    @alumnis = Alumni.all
+  end
 
-    def water_students
-        @water_students = Alumni.where(course: "Diploma in Water engineering")
-    end
+  def water_students
+    @water_students = Alumni.where(course: 'Diploma in Water engineering')
+  end
 
-    def electrical_students
-        @electrical_students = Alumni.where(course: "Diploma in Electrical Engineering")
-    end
+  def electrical_students
+    @electrical_students = Alumni.where(course: 'Diploma in Electrical Engineering')
+  end
 
-    def mechanical_students
-        @mechanical_students = Alumni.where(course: "Diploma in Mechanical Engineering")
-    end
+  def mechanical_students
+    @mechanical_students = Alumni.where(course: 'Diploma in Mechanical Engineering')
+  end
 
-    def architectural_students
-        @architectural_students = Alumni.where(course: "Diploma in Architecture")
-    end
+  def architectural_students
+    @architectural_students = Alumni.where(course: 'Diploma in Architecture')
+  end
 
-    def civil_students
-        @civil_students = Alumni.where(course: "Diploma in Civil Engineering")
-    end
+  def civil_students
+    @civil_students = Alumni.where(course: 'Diploma in Civil Engineering')
+  end
 
-    def cert_plumbing
-        @cert_plumbing = Alumni.where(course: "Certificate in Plumbing")
-    end
+  def cert_plumbing
+    @cert_plumbing = Alumni.where(course: 'Certificate in Plumbing')
+  end
 
-    def cert_electrical
-        @cert_electrical = Alumni.where(course: "Certificate in Electrical Installation")
-    end
+  def cert_electrical
+    @cert_electrical = Alumni.where(course: 'Certificate in Electrical Installation')
+  end
 
-    def cert_building
-        @cert_building = Alumni.where(course: "Certificate in Building and Concrete Practice")
-    end
+  def cert_building
+    @cert_building = Alumni.where(course: 'Certificate in Building and Concrete Practice')
+  end
 
-    def cert_motor_vehicle
-        @cert_motor_vehicle = Alumni.where(course: "Certificate in Motor Vehicle Maintenance")
-    end
-    private
-    def alumni_params
-        params.require(:alumni).permit(
-            :first_name, 
-            :last_name, 
-            :email,
-            :course,
-            :year_of_study,
-            :employed,
-            :employer,
-            :own_company,
-            :phonecontact
-        )
-    end
+  def cert_motor_vehicle
+    @cert_motor_vehicle = Alumni.where(course: 'Certificate in Motor Vehicle Maintenance')
+  end
+
+  private
+
+  def alumni_params
+    params.require(:alumni).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :course,
+      :year_of_study,
+      :employed,
+      :employer,
+      :own_company,
+      :phonecontact
+    )
+  end
 end
