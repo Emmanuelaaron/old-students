dc5fd267ee375eee358aee12a586d7a62c881fa0
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2022-03-05 00:47:12 +0300

    updates water engineering

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -3,10 +3,10 @@ class Alumni < ApplicationRecord
   validates :email, :phonecontact, uniqueness: true
 
   scope :dip_in_electrical, -> { where(course: 'Diploma in Electrical Engineering') }
-  scope :dip_in_water, -> { where(course: 'Diploma in Water Engineering') }
+  scope :dip_in_water, -> { where(course: 'Diploma in Water engineering') }
   scope :dip_in_mechanical, -> { where(course: 'Diploma in Mechanical Engineering') }
   scope :dip_in_architecture, -> { where(course: 'Diploma in Architecture') }
-  scope :dip_in_civil, -> { where(course: 'Diploma in Civil engineering') }
+  scope :dip_in_civil, -> { where(course: 'Diploma in Civil Engineering') }
   scope :cert_in_plumbing, -> { where(course: 'Certificate in Plumbing') }
   scope :cert_in_electrical, -> { where(course: 'Certificate in Electrical Installation') }
   scope :cert_building, -> { where(course: 'Certificate in Building and Concrete Practice') }
