1521864e6d097b3d9630e341d2ebc9caf6e6a99d
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2022-03-04 23:16:42 +0300

    adds login to the page

--- Gemfile
+++ Gemfile
@@ -21,11 +21,14 @@ gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 gem 'bootstrap', '~> 5.1.3'
+gem 'font-awesome-rails'
+
+gem 'hirb'
 
 gem 'jquery-rails'
 
@@ -42,8 +45,8 @@ group :development do
   gem 'listen', '~> 3.2'
   gem 'web-console', '>= 3.3.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'rubocop', '>= 1.0', '< 2.0'
   gem 'figaro'
+  gem 'rubocop', '>= 1.0', '< 2.0'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
--- app/controllers/alumnis_controller.rb
+++ app/controllers/alumnis_controller.rb
@@ -1,4 +1,45 @@
 class AlumnisController < ApplicationController
+  before_action :authenticate_user!, except: %i[new create welcome]
+  def welcome
+    @summaries = {}
+
+    @summaries['Diploma in Electrical Engineering'] = {}
+    @summaries['Diploma in Electrical Engineering']['total'] = Alumni.dip_in_electrical.count
+    @summaries['Diploma in Electrical Engineering']['link'] = alumniss_electrical_path
+
+    @summaries['Diploma in Water Engineering'] = {}
+    @summaries['Diploma in Water Engineering']['total'] = Alumni.dip_in_water.count
+    @summaries['Diploma in Water Engineering']['link'] = alumniss_water_path
+
+    @summaries['Diploma in Mechanical Engineering'] = {}
+    @summaries['Diploma in Mechanical Engineering']['total'] = Alumni.dip_in_mechanical.count
+    @summaries['Diploma in Mechanical Engineering']['link'] = alumniss_mechanical_path
+
+    @summaries['Diploma in Architecture'] = {}
+    @summaries['Diploma in Architecture']['total'] = Alumni.dip_in_architecture.count
+    @summaries['Diploma in Architecture']['link'] = alumniss_architecture_path
+
+    @summaries['Diploma in Civil Engineering'] = {}
+    @summaries['Diploma in Civil Engineering']['total'] = Alumni.dip_in_civil.count
+    @summaries['Diploma in Civil Engineering']['link'] = alumniss_civil_path
+
+    @summaries['Certificate in Plumbing'] = {}
+    @summaries['Certificate in Plumbing']['total'] = Alumni.cert_in_plumbing.count
+    @summaries['Certificate in Plumbing']['link'] = alumniss_cert_plumbing_path
+
+    @summaries['Certificate in Electrical Installation'] = {}
+    @summaries['Certificate in Electrical Installation']['total'] = Alumni.cert_in_electrical.count
+    @summaries['Certificate in Electrical Installation']['link'] = alumniss_cert_electrical_path
+
+    @summaries['Certificate in Building and Concrete Practice'] = {}
+    @summaries['Certificate in Building and Concrete Practice']['total'] = Alumni.cert_building.count
+    @summaries['Certificate in Building and Concrete Practice']['link'] = alumniss_cert_building_path
+
+    @summaries['Certificate in Motor Vehicle Maintenance'] = {}
+    @summaries['Certificate in Motor Vehicle Maintenance']['total'] = Alumni.cert_motor_vehicle.count
+    @summaries['Certificate in Motor Vehicle Maintenance']['link']= alumniss_cert_motor_vehicle_path
+  end
+
   def new
     @alumni = Alumni.new
   end
@@ -22,39 +63,39 @@ class AlumnisController < ApplicationController
   end
 
   def water_students
-    @water_students = Alumni.where(course: 'Diploma in Water engineering')
+    @water_students = Alumni.dip_in_water
   end
 
   def electrical_students
-    @electrical_students = Alumni.where(course: 'Diploma in Electrical Engineering')
+    @electrical_students = Alumni.dip_in_electrical
   end
 
   def mechanical_students
-    @mechanical_students = Alumni.where(course: 'Diploma in Mechanical Engineering')
+    @mechanical_students = Alumni.dip_in_mechanical
   end
 
   def architectural_students
-    @architectural_students = Alumni.where(course: 'Diploma in Architecture')
+    @architectural_students = Alumni.dip_in_architecture
   end
 
   def civil_students
-    @civil_students = Alumni.where(course: 'Diploma in Civil Engineering')
+    @civil_students = Alumni.dip_in_civil
   end
 
   def cert_plumbing
-    @cert_plumbing = Alumni.where(course: 'Certificate in Plumbing')
+    @cert_plumbing = Alumni.cert_in_plumbing
   end
 
   def cert_electrical
-    @cert_electrical = Alumni.where(course: 'Certificate in Electrical Installation')
+    @cert_electrical = Alumni.cert_in_electrical
   end
 
   def cert_building
-    @cert_building = Alumni.where(course: 'Certificate in Building and Concrete Practice')
+    @cert_building = Alumni.cert_building
   end
 
   def cert_motor_vehicle
-    @cert_motor_vehicle = Alumni.where(course: 'Certificate in Motor Vehicle Maintenance')
+    @cert_motor_vehicle = Alumni.cert_motor_vehicle
   end
 
   private
